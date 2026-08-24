097d9c85d2ee1c56ceadc1fe476b286decd89d3c
Aqaaab <abohmod875@gmail.com>
2026-08-24 08:39:06 +0300

    Add Android application module

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.aqaab.subtide"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.aqaab.subtide"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
+
+kotlin { jvmToolchain(17) }
+
