f6bc487e86db6198e44e7e4d5c1999fd9ff93b21
Aqaaab <abohmod875@gmail.com>
2026-08-24 08:44:12 +0300

    Add on-device Whisper and Arabic translation dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,14 @@ android {
         versionCode = 1
         versionName = "0.1.0"
     }
+
+    packaging { resources.excludes += "/META-INF/{AL2.0,LGPL2.1}" }
 }
 
 kotlin { jvmToolchain(17) }
 
+dependencies {
+    implementation("com.github.k2-fsa:sherpa-onnx:v1.13.4")
+    implementation("com.google.mlkit:translate:17.0.3")
+    implementation("com.google.mlkit:language-id:17.0.6")
+}
